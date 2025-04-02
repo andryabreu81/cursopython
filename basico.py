33f9cf2a4f4eaf9bbb3bb745806f80a54d24dac6
andryabreu81 <andryabreu@gmail.com>
2025-04-02 08:51:39 -0400

    numero mayor o menor

--- basico.py
+++ basico.py
@@ -3,4 +3,9 @@ num1 = 5
 num2 = 10
 suma = num1 + num2
 print("La suma de", num1, "y", num2, "es:", suma)
+
+if num1 > num2:
+    print(num1, "es mayor que", num2)
+else:
+    print(num2, "es mayor que", num1)
 # Esto es un comentario
