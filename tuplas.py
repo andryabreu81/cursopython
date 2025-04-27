0500283881173224688d681152cfe373ce157d95
andryabreu81 <andryabreu@gmail.com>
2025-04-27 12:14:07 -0400

    avance 1 tuplas

---
+++ tuplas.py
@@ -0,0 +1,19 @@
+# ejemplos y ejercicios de tuplas
+# Definicion de tuplas
+# es una coleccion de elementos inmutables ordenados
+# se definen con parentesis ()
+# se pueden almacenar diferentes tipos de datos
+# se pueden almacenar listas dentro de tuplas
+# se pueden almacenar tuplas dentro de listas
+# se pueden almacenar tuplas dentro de tuplas
+
+tupla = ('naranjas','manzanas','peras','kiwis','mangos')
+print(tupla)# imprime la tupla
+print(type(tupla))# imprime el tipo de dato
+print(len(tupla))# imprime la longitud de la tupla
+print(tupla[0])# imprime el primer elemento de la tupla
+print(tupla[0:2])# imprime parte de la tupla
+
+# como cambiar los datos de una tupla
+tecnologias = ('PHP','Python','Angular','Java','JavaScript','SQL')
+print(tecnologias)# imprime la tupla
