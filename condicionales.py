5b5ea6a0e0f8b73a5965a66149958b5e7a87fa70
andryabreu81 <andryabreu@gmail.com>
2025-04-28 11:33:22 -0400

    condicionales

---
+++ condicionales.py
@@ -0,0 +1,13 @@
+## condicionales if elif else
+# Condicionales
+
+a = 5
+b = 2
+c = 3
+
+if a > b:
+    print("a es mayor que b")
+elif c < b:
+    print("c es menor que b")
+else:
+    print("a es igual a c")
