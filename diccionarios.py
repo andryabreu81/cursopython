0216be82d288129ca8c43cb1aedaebf380cf32da
andryabreu81 <andryabreu@gmail.com>
2025-04-29 11:20:48 -0400

    mejoras en taller diccionario

--- diccionarios.py
+++ diccionarios.py
@@ -53,3 +53,38 @@ print('6. items')
 # items: nos decuelve una tupla de clave:valor en una lista
 items = consDiccionario2.items()
 print(items)
+
+# agregar elementos a un diccionario
+print('7. agregar elementos a un diccionario')
+consDiccionario2['cedula'] = '15837503'
+print(consDiccionario2)
+
+# agregar un elemento a una lista dentro de un diccionario
+print('7.1 agregar un elemento a una lista dentro de un diccionario')
+consDiccionario2['tecnologias'].append('java')
+print(consDiccionario2)
+# agregar un elemento a un diccionario dentro de un diccionario
+print('7.2 agregar un elemento a un diccionario dentro de un diccionario')
+consDiccionario2['direccion']['ciudad'] = 'Caracas'
+print(consDiccionario2)
+
+# eliminar elementos de un diccionario
+print('8. eliminar elementos de un diccionario')
+consDiccionario2.pop('apellido')
+print(consDiccionario2)
+
+# eliminar todos los elementos de un diccionario
+print('9. eliminar todos los elementos de un diccionario')
+consDiccionario2.clear()
+print(consDiccionario2)
+
+# eliminar un diccionario
+print('10. eliminar un diccionario')
+del consDiccionario2
+# print(consDiccionario2)  # imprime el diccionario
+
+# crear un diccionario vacio
+print('11. crear un diccionario vacio')
+diccionario_vacio = {}
+print(diccionario_vacio)  # imprime el diccionario
+
